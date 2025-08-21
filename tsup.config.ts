d84a37822dbec7795b4511ba130aa78b0ba444a8
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-20 21:48:29 -0400

    chore(build): emit cjs only

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,15 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig([
-  {
-    entry: {
-      index: 'src/index.ts',
-      ui: 'src/ui/index.ts',
-      hooks: 'src/hooks/index.ts',
-      providers: 'src/providers/index.ts',
-      polyfills: 'src/polyfills/index.ts',
-      'cli/main': 'src/cli/main.ts',
-    },
-    format: ['esm', 'cjs'],
-    dts: true,
-    sourcemap: true,
-    clean: true,
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts',
+    ui: 'src/ui/index.ts',
+    hooks: 'src/hooks/index.ts',
+    providers: 'src/providers/index.ts',
+    polyfills: 'src/polyfills/index.ts',
+    'cli/main': 'src/cli/main.ts',
   },
-])
+  format: ['cjs'],
+  dts: true,
+  sourcemap: true,
+})
