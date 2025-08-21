f2f5978bd3c1cc324ac5f8de5ece399c7c8e3c2c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-21 18:59:43 -0400

    fix: install restyle without expo install

--- src/cli/commands/install.ts
+++ src/cli/commands/install.ts
@@ -2,6 +2,7 @@ import { command } from 'cmd-ts'
 import { readFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { spawnSync as $ } from 'picospawn'
+import { pick } from 'radashi'
 
 export default command({
   name: 'install',
@@ -11,16 +12,25 @@ export default command({
     // Read nativ's package.json to get peer dependencies
     const packageJsonPath = join(__dirname, '../../package.json')
     const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf-8'))
-
     const peerDeps = packageJson.peerDependencies || {}
+
+    // Use our fork of Restyle.
     peerDeps['@shopify/restyle'] =
       'npm:@alloc/restyle@' + peerDeps['@shopify/restyle']
 
-    // Install expo first
-    console.log(`\npnpm add expo@"${peerDeps.expo}"`)
-    $('pnpm add', [`expo@${peerDeps.expo}`])
+    // Install expo first, so we can run expo install. Also include
+    // any dependencies using the "npm:" protocol (not supported by
+    // expo install).
+    const initialDeps = pick(peerDeps, ['expo', '@shopify/restyle'])
 
-    delete peerDeps.expo
+    $(
+      'pnpm add',
+      Object.entries(initialDeps).map(([key, value]) => `${key}@${value}`)
+    )
+
+    for (const key in initialDeps) {
+      delete peerDeps[key]
+    }
 
     // Get remaining dependencies
     const otherDeps = Object.entries(peerDeps).map(([key, value]) =>
