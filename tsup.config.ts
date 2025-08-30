17049521dd1066832a2da4cf2be876bb6cba3d5a
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-29 21:19:37 -0400

    chore(build): switch to .ts/.d.ts only; no .js outputs

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,20 +4,27 @@ import { defineConfig } from 'tsup'
 
 const components = fs.readdirSync(path.join(__dirname, 'src/ui'))
 
-export default defineConfig({
-  entry: {
-    ...components.reduce((acc, component) => {
-      const type = path.extname(component)
-      const name = `ui/${path.basename(component, type)}`
-      acc[name] = `src/ui/${component}`
-      return acc
-    }, {}),
-    providers: 'src/providers/index.ts',
-    hooks: 'src/hooks/index.ts',
-    polyfills: 'src/polyfills/index.ts',
-    'cli/main': 'src/cli/main.ts',
+export default defineConfig([
+  {
+    entry: {
+      ...components.reduce((acc, component) => {
+        const type = path.extname(component)
+        const name = `ui/${path.basename(component, type)}`
+        acc[name] = `src/ui/${component}`
+        return acc
+      }, {}),
+      providers: 'src/providers/index.ts',
+      hooks: 'src/hooks/index.ts',
+      polyfills: 'src/polyfills/index.ts',
+    },
+    format: ['cjs'],
+    dts: { only: true },
   },
-  format: ['cjs'],
-  dts: true,
-  sourcemap: true,
-})
+  {
+    entry: {
+      'cli/main': 'src/cli/main.ts',
+    },
+    format: ['cjs'],
+    sourcemap: true,
+  },
+])
